689d71f31ecb46c2d44921dfc1205f29f543585f
Robert-Munroe <rmunroe@student.bridgew.edu>
2021-02-25 10:50:59 -0500

    Fixed test for database

--- tests/test_Projct1.py
+++ tests/test_Projct1.py
@@ -6,15 +6,23 @@ def test_get_data():
     assert len(results) >= 100
 
 
-def test_pop_database():
-    data = "test data"
-    conn, cursor = RmunroeAPIKey.open_db("test_database.sqlite")
-    print(type(conn))
+def test_data_save():
+    # first lets add test data
+    conn, cursor = RmunroeAPIKey.open_db('testdb.sqlite')
     RmunroeAPIKey.setup_db(cursor)
-
-    RmunroeAPIKey.insert_data(cursor, data)
-
-    data_in_table = RmunroeAPIKey.select_data(cursor, data)
+    test_data = [{'school.name': 'Test University', '2018.student.size': 1000, 'school.state': 'MA', 'id': 11001,
+                  '2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line': 456,
+                  '2016.repayment.3_yr_repayment.overall': 4004}]
+    RmunroeAPIKey.insert_data(test_data, cursor)
     RmunroeAPIKey.close_db(conn)
-
-    assert data_in_table == data
+    # test data is saved - now lets see if it is there
+    conn, cursor = RmunroeAPIKey.open_db('testdb.sqlite')
+    # the sqlite_master table is a metadata table with information about all the tables in it
+    cursor.execute('''SELECT name FROM sqlite_master
+    WHERE type ='table' AND name LIKE 'university_%';''')  # like does pattern matching with % as the wildcard
+    results = cursor.fetchall()
+    assert len(results) == 1
+    cursor.execute(''' SELECT university_name FROM university_data''')
+    results = cursor.fetchall()
+    test_record = results[0]
+    assert test_record[0] == 'Test University'
